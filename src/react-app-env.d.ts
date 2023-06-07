70b7949e2fb6fb79a65f40c4710a2ccf226a56d6
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-06-07 17:31:51 +0200

    env variables

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'uat' | 'production';
 
+    REACT_APP_API_MOCK_MERCHANTS: string;
     REACT_APP_API_MOCK_PARTIES: string;
     REACT_APP_API_MOCK_PRODUCTS: string;
     REACT_APP_API_MOCK_ROLE_PERMISSION: string;
