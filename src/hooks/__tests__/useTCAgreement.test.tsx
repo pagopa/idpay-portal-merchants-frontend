d84ec4f416a5e73d7afb0eac5ac38af5037d21a0
Oraldo Doci <oraldo.doci@nttdata.com>
2023-06-14 11:23:07 +0200

    test for useTCAgreement hook

--- src/hooks/__tests__/useTCAgreement.test.tsx
+++ src/hooks/__tests__/useTCAgreement.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { RolePermissionApiMocked } from '../../api/__mocks__/rolePermissionApiClient';
+import { PortalConsentDTO } from '../../api/generated/role-permission/PortalConsentDTO';
 import { renderWithContext } from '../../utils/__tests__/test-utils';
 import useTCAgreement from '../useTCAgreement';
 
@@ -19,7 +21,19 @@ const HookWrapper = () => {
 };
 
 describe('test suite for usTCAgreement hook', () => {
-  test('test call of useTCAgreement hook', () => {
+  test('call of useTCAgreement hook', () => {
+    renderWithContext(<HookWrapper />);
+  });
+
+  test('mock api call getPortalConsent and render hook useTCAgreement', async () => {
+    RolePermissionApiMocked.getPortalConsent = async (): Promise<PortalConsentDTO> =>
+      new Promise((resolve) => resolve({ firstAcceptance: true, versionId: 'version123' }));
+    renderWithContext(<HookWrapper />);
+  });
+
+  test('mock catch case of  api call getPortalConsent and render hook useTCAgreement', async () => {
+    RolePermissionApiMocked.getPortalConsent = async (): Promise<PortalConsentDTO> =>
+      Promise.reject('mocked error response for tests');
     renderWithContext(<HookWrapper />);
   });
 });
