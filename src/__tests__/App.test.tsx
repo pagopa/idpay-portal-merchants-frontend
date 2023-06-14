16e19554ed2143394b09824b26f11f005f291629
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-06-14 11:23:41 +0200

    Test suite for app component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,32 +1,24 @@
 /* istanbul ignore file */
-import { render /* , screen, waitFor */, waitFor } from '@testing-library/react';
-import App from '../App';
-import { Provider } from 'react-redux';
-import { createStore, store } from '../redux/store';
 
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router';
-import { theme } from '@pagopa/mui-italia';
+import App from '../App';
 import '../locale';
 import React from 'react';
-import { ThemeProvider } from '@mui/system';
-// import { PartiesState } from '../redux/slices/partiesSlice';
+import { renderWithContext } from '../utils/__tests__/test-utils';
 
 jest.mock('@pagopa/mui-italia/dist/components/Footer/Footer', () => ({
   Footer: () => {},
 }));
 
-const mockSignOutFn = jest.fn();
-
-jest.mock('../hooks/useTCAgreement', () => () => ({
-  isTOSAccepted: true,
-  acceptTOS: mockSignOutFn,
-  firstAcceptance: false,
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: 'localhost:3000/portale-esercenti',
+  }),
 }));
 
+const mockSignOutFn = jest.fn();
+
 jest.mock('../decorators/withLogin');
-jest.mock('../decorators/withParties');
-jest.mock('../decorators/withSelectedParty');
 jest.mock('../decorators/withSelectedPartyProducts');
 
 beforeEach(() => {
@@ -34,32 +26,17 @@ beforeEach(() => {
   jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
 
-const renderApp = (
-  injectedStore?: ReturnType<typeof createStore>,
-  injectedHistory?: ReturnType<typeof createMemoryHistory>
-) => {
-  const store = injectedStore ? injectedStore : createStore();
-  const history = injectedHistory ? injectedHistory : createMemoryHistory();
+describe('Test suite for App component', () => {
+  test('Render App with TOS accepted', () => {
+    renderWithContext(<App />);
+  });
 
-  render(
-    <ThemeProvider theme={theme}>
-      <Router history={history}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Router>
-    </ThemeProvider>
-  );
-  return { store, history };
-};
-
-test('Test rendering dashboard parties loaded', () => {
-  const history = createMemoryHistory();
-  history.push('/dashboard/6');
-});
-
-test('Test routing ', async () => {
-  const history = createMemoryHistory();
-  renderApp();
-  await waitFor(() => expect(history.location.pathname).toBe('/'));
+  test('Render App with TOS not accepted', () => {
+    jest.mock('../hooks/useTCAgreement', () => () => ({
+      isTOSAccepted: true,
+      acceptTOS: mockSignOutFn,
+      firstAcceptance: false,
+    }));
+    renderWithContext(<App />);
+  });
 });
