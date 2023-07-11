de2e9733c1163802c2803e2c5265788af524d517
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-07-11 14:28:02 +0200

    Added filters on lists

--- src/pages/initiativeDiscounts/MerchantTransactionsProcessed.tsx
+++ src/pages/initiativeDiscounts/MerchantTransactionsProcessed.tsx
@@ -1,11 +1,17 @@
 import {
   Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TablePagination,
   TableRow,
+  TextField,
 } from '@mui/material';
 import { itIT } from '@mui/material/locale';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -13,9 +19,11 @@ import useErrorDispatcher from '@pagopa/selfcare-common-frontend/hooks/useErrorD
 import useLoading from '@pagopa/selfcare-common-frontend/hooks/useLoading';
 import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ButtonNaked } from '@pagopa/mui-italia';
+import { useFormik } from 'formik';
 import { formatDate, formattedCurrency } from '../../helpers';
 import { getMerchantTransactionsProcessed } from '../../services/merchantService';
-import { pagesTableContainerStyle } from '../../styles';
+import { genericContainerStyle, pagesTableContainerStyle } from '../../styles';
 import EmptyList from '../components/EmptyList';
 import { MerchantTransactionProcessedDTO } from '../../api/generated/merchants/MerchantTransactionProcessedDTO';
 import { renderTrasactionProcessedStatus } from './helpers';
@@ -30,9 +38,27 @@ const MerchantTransactionsProcessed = ({ id }: Props) => {
   const [rows, setRows] = useState<Array<MerchantTransactionProcessedDTO>>([]);
   const [rowsPerPage, setRowsPerPage] = useState<number>(0);
   const [totalElements, setTotalElements] = useState<number>(0);
+  const [filterByUser, setFilterByUser] = useState<string | undefined>();
+  const [filterByStatus, setFilterByStatus] = useState<string | undefined>();
   const theme = createTheme(itIT);
   const setLoading = useLoading('GET_INITIATIVE_MERCHANT_DISCOUNTS_LIST');
   const addError = useErrorDispatcher();
+
+  const formik = useFormik({
+    initialValues: {
+      searchUser: '',
+      filterStatus: '',
+    },
+    onSubmit: (values) => {
+      if (typeof id === 'string') {
+        const fU = values.searchUser.length > 0 ? values.searchUser : undefined;
+        const fS = values.filterStatus.length > 0 ? values.filterStatus : undefined;
+        setFilterByUser(fU);
+        setFilterByStatus(fS);
+        getTableData(id, 0, fU, fS);
+      }
+    },
+  });
 
   const getTableData = (
     initiativeId: string,
@@ -68,14 +94,27 @@ const MerchantTransactionsProcessed = ({ id }: Props) => {
       .finally(() => setLoading(false));
   };
 
+  const resetForm = () => {
+    const initialValues = { searchUser: '', filterStatus: '' };
+    formik.resetForm({ values: initialValues });
+    setFilterByUser(undefined);
+    setFilterByStatus(undefined);
+    setRows([]);
+    if (typeof id === 'string') {
+      getTableData(id, 0, undefined, undefined);
+    }
+  };
+
   useMemo(() => {
     setPage(0);
+    setFilterByUser(undefined);
+    setFilterByStatus(undefined);
   }, [id]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
     if (typeof id === 'string') {
-      getTableData(id, page, undefined, undefined);
+      getTableData(id, page, filterByUser, filterByStatus);
     }
     return () => {
       setRows([]);
@@ -92,6 +131,60 @@ const MerchantTransactionsProcessed = ({ id }: Props) => {
 
   return (
     <Box sx={{ width: '100%' }}>
+      <Box sx={{ ...genericContainerStyle, gap: 2, alignItems: 'baseline' }}>
+        <FormControl sx={{ gridColumn: 'span 4' }}>
+          <TextField
+            label={t('pages.initiativeDiscounts.searchByFiscalCode')}
+            placeholder={t('pages.initiativeDiscounts.searchByFiscalCode')}
+            name="searchUser"
+            aria-label="searchUser"
+            role="input"
+            InputLabelProps={{ required: false }}
+            value={formik.values.searchUser}
+            onChange={(e) => formik.handleChange(e)}
+            size="small"
+            data-testid="searchUser-test"
+          />
+        </FormControl>
+        <FormControl sx={{ gridColumn: 'span 2' }} size="small">
+          <InputLabel>{t('pages.initiativeDiscounts.filterByStatus')}</InputLabel>
+          <Select
+            id="filterStatus"
+            inputProps={{
+              'data-testid': 'filterStatus-select',
+            }}
+            name="filterStatus"
+            label={t('pages.initiativeDiscounts.filterByStatus')}
+            placeholder={t('pages.initiativeDiscounts.filterByStatus')}
+            onChange={(e) => formik.handleChange(e)}
+            value={formik.values.filterStatus}
+          >
+            <MenuItem value={'REWARDED'}>{t('commons.discountStatusEnum.rewarded')}</MenuItem>
+            <MenuItem value={'CANCELLED'}>{t('commons.discountStatusEnum.cancelled')}</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ gridColumn: 'span 1' }}>
+          <Button
+            sx={{ height: '44.5px' }}
+            variant="outlined"
+            size="small"
+            onClick={() => formik.handleSubmit()}
+            data-testid="apply-filters-test"
+          >
+            {t('commons.filterBtn')}
+          </Button>
+        </FormControl>
+        <FormControl sx={{ gridColumn: 'span 1' }}>
+          <ButtonNaked
+            component="button"
+            sx={{ color: 'primary.main', fontWeight: 600, fontSize: '0.875rem' }}
+            onClick={resetForm}
+          >
+            {t('commons.removeFiltersBtn')}
+          </ButtonNaked>
+        </FormControl>
+      </Box>
+
       {rows.length > 0 ? (
         <Box
           sx={{
