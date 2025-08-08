9ec4fce87344433ba2c32af55e16dc813d0c2e81
escarsel <eloisa.scarsella@nttdata.com>
2025-08-08 14:48:52 +0200

    eliminato codice commentato

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -5,11 +5,6 @@ export function formatDate(value: any) {
     return format(value, 'dd/MM/yyyy HH:mm');
 }
 
-// export const curFormatter = new Intl.NumberFormat('en-US', {
-//   style: 'currency',
-//   currency: 'USD',
-// });
-
 export const curFormatter = (amount: number): string => Intl.NumberFormat('it-EU', {style: 'currency', currency: 'EUR'}).format(amount);
 
 
