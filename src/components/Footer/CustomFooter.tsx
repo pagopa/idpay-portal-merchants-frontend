0bdff301c3e845e1b2ef20cb2ac4cfd6cdcec998
fragreco <fragreco@emeal.nttdata.com>
2025-10-17 16:19:30 +0200

    fix: env

--- src/components/Footer/CustomFooter.tsx
+++ src/components/Footer/CustomFooter.tsx
@@ -3,9 +3,9 @@ import { FooterPostLogin, FooterLegal } from '@pagopa/mui-italia';
 
 const FOOTER_LINKS = {
     COMPANY: 'https://www.pagopa.it/it/',
-    PRIVACY: 'https://welfare.dev.cstar.pagopa.it/portale-esercenti/privacy-policy',
+    PRIVACY: 'https://welfare.cstar.pagopa.it/portale-esercenti/privacy-policy',
     PERSONAL_DATA: 'https://privacyportal-de.onetrust.com/webform/77f17844-04c3-4969-a11d-462ee77acbe1/9ab6533d-be4a-482e-929a-0d8d2ab29df8',
-    TERMS_AND_CONDITIONS: 'https://welfare.dev.cstar.pagopa.it/portale-esercenti/terms-of-service',
+    TERMS_AND_CONDITIONS: 'https://welfare.cstar.pagopa.it/portale-esercenti/terms-of-service',
     A11Y: 'https://form.agid.gov.it/view/87f46790-9798-11f0-b583-8b5f76942354'
 } as const;
 
