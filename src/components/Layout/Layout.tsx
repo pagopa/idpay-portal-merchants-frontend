c7d43d92c783dc85a796d9b6e43dd291e83f5912
Eleonora <cossu.eleonora1996@gmail.com>
2025-10-21 12:22:58 +0200

    fix: [EBE-308] header update (#172)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,12 +5,12 @@ import { useSelector } from 'react-redux';
 import { userSelectors } from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
 import { useLocation } from 'react-router-dom';
 import { matchPath } from 'react-router';
-import Header from '../Header/Header';
 import SideMenu from '../SideMenu/SideMenu';
 import ROUTES from '../../routes';
 import routes from '../../routes';
 import { useInitiativesList } from '../../hooks/useInitiativesList';
 import Footer from '../Footer/Footer';
+import CustomHeader from '../Header/CustomHeader';
 
 type Props = {
   children?: React.ReactNode;
@@ -51,7 +51,7 @@ const Layout = ({ children }: Props) => {
       minHeight="100vh"
     >
       <Box gridArea="header">
-        <Header
+        <CustomHeader
           withSecondHeader={showAssistanceInfo}
           onExit={onExit}
           loggedUser={loggedUser}
