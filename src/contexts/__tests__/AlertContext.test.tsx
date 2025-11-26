92b88f81fece9490ad2c1c08782f976a1c7bfe11
EleonoraC96 <cossu.eleonora1996@gmail.com>
2025-11-26 09:59:09 +0100

    fix: [EBE-297] AlertContext tests

---
+++ src/contexts/__tests__/AlertContext.test.tsx
@@ -0,0 +1,69 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { AlertProvider } from "../AlertContext";
+import { useAlert } from "../../hooks/useAlert";
+import { useEffect } from "react";
+import Layout from '../../components/Layout/Layout'
+import UnloadEventHandler from "@pagopa/selfcare-common-frontend/components/UnloadEventHandler";
+import { Provider } from "react-redux";
+import { createStore } from "../../redux/store";
+import LoadingOverlay from "@pagopa/selfcare-common-frontend/components/Loading/LoadingOverlay";
+import UserNotifyHandle from "@pagopa/selfcare-common-frontend/components/UserNotifyHandle";
+import AlertComponent from "../../components/Alert/AlertComponent";
+
+const EmptyConsumer = () => {
+    const { title, text, isOpen, setAlert } = useAlert();
+
+    useEffect(() => {
+        setAlert()
+    }, [])
+
+    return <div>{ isOpen && <AlertComponent data-testid="alert-test" title={title} message={text} /> }</div>
+}
+
+const Consumer = () => {
+    const { title, text, isOpen, setAlert } = useAlert();
+
+    useEffect(() => {
+        setAlert('title', 'text', true)
+    }, [])
+
+    return <div>{ isOpen && <AlertComponent data-testid="alert-test" title={title} message={text} /> }</div>
+}
+
+describe('AlertContext', () => {
+
+  it('renders AlertContext with empty values', () => {
+
+    render(
+        <AlertProvider>
+            <EmptyConsumer />
+        </AlertProvider>
+    );
+
+    const alert = document.querySelector('[data-testid="alert-test"') as HTMLElement;
+
+    expect(alert).not.toBeInTheDocument()
+
+  });
+
+  it('renders AlertContext with populated state', async () => {
+
+    render(
+        <AlertProvider>
+            <Consumer />
+        </AlertProvider>
+    );
+
+    const alert = document.querySelector('[data-testid="alert-test"') as HTMLElement;
+
+    expect(screen.getByText('title')).toBeInTheDocument()
+    expect(screen.getByText('text')).toBeInTheDocument()
+
+    await waitFor(() => {
+      setTimeout(() => {
+        expect(alert).not.toBeInTheDocument()
+      }, 5000)
+    })
+
+  });
+});
