81dd4d562aae63bb7fbef3d7da31143e1f3fc71a
fragreco01 <fragreco@emeal.nttdata.com>
2026-06-19 15:29:28 +0200

    fix: error message restored

--- src/pages/initiativeStores/initiativeStoresUpload.tsx
+++ src/pages/initiativeStores/initiativeStoresUpload.tsx
@@ -133,8 +133,8 @@ const InitiativeStoresUpload: React.FC = () => {
       if (response) {
         if (response?.code === 'POINT_OF_SALE_ALREADY_REGISTERED') {
           setAlert({
-            title: t('errors.pointOfSaleAlreadyExistsError'),
-            text: t('errors.pointOfSaleAlreadyExistsDescription'),
+            title: t('errors.duplicateEmailError'),
+            text: `${response?.message} già associata ad altro punto vendita`,
             isOpen: true,
             severity: 'error',
           });
