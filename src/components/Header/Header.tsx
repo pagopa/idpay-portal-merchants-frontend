cc03b03c919ec7b4604f1e9f095b3fbcfd37b933
EleonoraC96 <cossu.eleonora1996@gmail.com>
2026-07-16 10:40:04 +0200

    fix: [BND-1200] commented unused code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,126 +1,126 @@
-import { ProductEntity } from '@pagopa/mui-italia';
-import { Header as CommonHeader } from '@pagopa/selfcare-common-frontend/lib';
-import { User } from '@pagopa/selfcare-common-frontend/lib/model/User';
-import { trackEvent } from '@pagopa/selfcare-common-frontend/lib/services/analyticsService';
-import { CONFIG } from '@pagopa/selfcare-common-frontend/lib/config/env';
-import { useEffect, useMemo, useState } from 'react';
-import { WithPartiesProps } from '../../decorators/withParties';
-import { Product } from '../../model/Product';
-import { useAppSelector } from '../../redux/hooks';
-import { partiesSelectors } from '../../redux/slices/partiesSlice';
-import { Party } from '../../model/Party';
-import { ENV } from '../../utils/env';
-import { browserConsole } from '../../utils/consoleLogger';
-import useScopedTranslation from '../../hooks/useScopedTranslation';
-import { useUserPermissions } from '../../hooks/useUserPermissions';
+// import { ProductEntity } from '@pagopa/mui-italia';
+// import { Header as CommonHeader } from '@pagopa/selfcare-common-frontend/lib';
+// import { User } from '@pagopa/selfcare-common-frontend/lib/model/User';
+// import { trackEvent } from '@pagopa/selfcare-common-frontend/lib/services/analyticsService';
+// import { CONFIG } from '@pagopa/selfcare-common-frontend/lib/config/env';
+// import { useEffect, useMemo, useState } from 'react';
+// import { WithPartiesProps } from '../../decorators/withParties';
+// import { Product } from '../../model/Product';
+// import { useAppSelector } from '../../redux/hooks';
+// import { partiesSelectors } from '../../redux/slices/partiesSlice';
+// import { Party } from '../../model/Party';
+// import { ENV } from '../../utils/env';
+// import { browserConsole } from '../../utils/consoleLogger';
+// import useScopedTranslation from '../../hooks/useScopedTranslation';
+// import { useUserPermissions } from '../../hooks/useUserPermissions';
 
-type Props = WithPartiesProps & {
-  withSecondHeader: boolean;
-  onExit: (exitAction: () => void) => void;
-  loggedUser?: User;
-};
+// type Props = WithPartiesProps & {
+//   withSecondHeader: boolean;
+//   onExit: (exitAction: () => void) => void;
+//   loggedUser?: User;
+// };
 
-const Header = ({ withSecondHeader, onExit, loggedUser }: /* , parties */ Props) => {
-  const { t } = useScopedTranslation();
-  const { role } = useUserPermissions();
-  const products = useAppSelector(partiesSelectors.selectPartySelectedProducts);
-  const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
-  const [party2Show, setParty2Show] = useState<Array<Party>>();
-  const title = t('commons.title');
+// const Header = ({ withSecondHeader, onExit, loggedUser }: /* , parties */ Props) => {
+//   const { t } = useScopedTranslation();
+//   const { role } = useUserPermissions();
+//   const products = useAppSelector(partiesSelectors.selectPartySelectedProducts);
+//   const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
+//   const [party2Show, setParty2Show] = useState<Array<Party>>();
+//   const title = t('commons.title');
 
-  const welfareProduct: ProductEntity = {
-    // TODO check if correct
-    id: 'prod-idpay-merchants',
-    title,
-    productUrl: CONFIG.HEADER.LINK.PRODUCTURL,
-    linkType: 'internal',
-  };
+//   const welfareProduct: ProductEntity = {
+//     // TODO check if correct
+//     id: 'prod-idpay-merchants',
+//     title,
+//     productUrl: CONFIG.HEADER.LINK.PRODUCTURL,
+//     linkType: 'internal',
+//   };
 
-  useEffect(() => setParty2Show([{ ...(selectedParty as Party) }]), [selectedParty]);
+//   useEffect(() => setParty2Show([{ ...(selectedParty as Party) }]), [selectedParty]);
 
-  // const parties2Show = parties.filter((party) => party.status === 'ACTIVE');
-  const activeProducts: Array<Product> = useMemo(
-    () =>
-      [
-        {
-          id: welfareProduct.id,
-          title: welfareProduct.title,
-          publicUrl: welfareProduct.productUrl,
-        } as unknown as Product,
-      ].concat(
-        products?.filter(
-          (p) => p.id !== welfareProduct.id && p.status === 'ACTIVE' && p.authorized
-        ) ?? []
-      ),
-    [products]
-  );
+//   // const parties2Show = parties.filter((party) => party.status === 'ACTIVE');
+//   const activeProducts: Array<Product> = useMemo(
+//     () =>
+//       [
+//         {
+//           id: welfareProduct.id,
+//           title: welfareProduct.title,
+//           publicUrl: welfareProduct.productUrl,
+//         } as unknown as Product,
+//       ].concat(
+//         products?.filter(
+//           (p) => p.id !== welfareProduct.id && p.status === 'ACTIVE' && p.authorized
+//         ) ?? []
+//       ),
+//     [products]
+//   );
 
-  const toRoleLabel = (roleKey?: string) => {
-    if (!roleKey) {
-      return '';
-    }
-    const normalizedRole = roleKey.toLowerCase();
-    const translationKey = `roles.${normalizedRole}`;
-    const translated = t(translationKey);
-    return translated !== translationKey ? translated : roleKey;
-  };
+//   const toRoleLabel = (roleKey?: string) => {
+//     if (!roleKey) {
+//       return '';
+//     }
+//     const normalizedRole = roleKey.toLowerCase();
+//     const translationKey = `roles.${normalizedRole}`;
+//     const translated = t(translationKey);
+//     return translated !== translationKey ? translated : roleKey;
+//   };
 
-  const effectiveRoleLabel = toRoleLabel(role);
+//   const effectiveRoleLabel = toRoleLabel(role);
 
-  return (
-    <CommonHeader
-      onExit={onExit}
-      onDocumentationClick={() => window.open(ENV.CONFIG.HEADER.OPERATION_MANUAL_LINK, '_blank')}
-      withSecondHeader={withSecondHeader}
-      selectedPartyId={selectedParty?.partyId}
-      selectedProductId={welfareProduct.id}
-      addSelfcareProduct={false} // TODO verify if returned from API
-      productsList={activeProducts.map((p) => ({
-        id: p.id,
-        title: p.title,
-        productUrl: p.urlPublic ?? '',
-        linkType: 'internal',
-      }))}
-      partyList={
-        party2Show &&
-        party2Show.map((party) => ({
-          id: party.partyId,
-          name: party.description,
-          productRole:
-            effectiveRoleLabel || party?.roles?.map((r) => toRoleLabel(r.roleKey)).join(','),
-          logoUrl: party.urlLogo,
-        }))
-      }
-      loggedUser={
-        loggedUser
-          ? {
-              id: loggedUser ? loggedUser.uid : '',
-              name: loggedUser?.name,
-              surname: loggedUser?.surname,
-              email: loggedUser?.email,
-            }
-          : false
-      }
-      assistanceEmail={ENV.ASSISTANCE.EMAIL}
-      enableLogin={true}
-      onSelectedProduct={(p) =>
-        onExit(() =>
-          browserConsole.log(`TODO: perform token exchange to change Product and set ${p}`)
-        )
-      }
-      onSelectedParty={(selectedParty: any) => {
-        if (selectedParty) {
-          trackEvent('PARTY_SELECTION', {
-            party_id: selectedParty.id,
-          });
-          onExit(() =>
-            browserConsole.log(
-              `TODO: perform token exchange to change Party and set ${selectedParty}`
-            )
-          );
-        }
-      }}
-    />
-  );
-};
-export default Header;
+//   return (
+//     <CommonHeader
+//       onExit={onExit}
+//       onDocumentationClick={() => window.open(ENV.CONFIG.HEADER.OPERATION_MANUAL_LINK, '_blank')}
+//       withSecondHeader={withSecondHeader}
+//       selectedPartyId={selectedParty?.partyId}
+//       selectedProductId={welfareProduct.id}
+//       addSelfcareProduct={false} // TODO verify if returned from API
+//       productsList={activeProducts.map((p) => ({
+//         id: p.id,
+//         title: p.title,
+//         productUrl: p.urlPublic ?? '',
+//         linkType: 'internal',
+//       }))}
+//       partyList={
+//         party2Show &&
+//         party2Show.map((party) => ({
+//           id: party.partyId,
+//           name: party.description,
+//           productRole:
+//             effectiveRoleLabel || party?.roles?.map((r) => toRoleLabel(r.roleKey)).join(','),
+//           logoUrl: party.urlLogo,
+//         }))
+//       }
+//       loggedUser={
+//         loggedUser
+//           ? {
+//               id: loggedUser ? loggedUser.uid : '',
+//               name: loggedUser?.name,
+//               surname: loggedUser?.surname,
+//               email: loggedUser?.email,
+//             }
+//           : false
+//       }
+//       assistanceEmail={ENV.ASSISTANCE.EMAIL}
+//       enableLogin={true}
+//       onSelectedProduct={(p) =>
+//         onExit(() =>
+//           browserConsole.log(`TODO: perform token exchange to change Product and set ${p}`)
+//         )
+//       }
+//       onSelectedParty={(selectedParty: any) => {
+//         if (selectedParty) {
+//           trackEvent('PARTY_SELECTION', {
+//             party_id: selectedParty.id,
+//           });
+//           onExit(() =>
+//             browserConsole.log(
+//               `TODO: perform token exchange to change Party and set ${selectedParty}`
+//             )
+//           );
+//         }
+//       }}
+//     />
+//   );
+// };
+// export default Header;
--- src/components/TOSLayout/__tests__/TOSLayout.test.tsx
+++ src/components/TOSLayout/__tests__/TOSLayout.test.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { renderWithContext } from '../../../utils/__tests__/test-utils';
-import TOSLayout from '../TOSLayout';
+// import React from 'react';
+// import { renderWithContext } from '../../../utils/__tests__/test-utils';
+// import TOSLayout from '../TOSLayout';
 
-beforeEach(() => {
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
-});
+// beforeEach(() => {
+//   jest.spyOn(console, 'warn').mockImplementation(() => {});
+// });
 
-describe('test suite for TOSLayout', () => {
-  test('test render TosLayout', () => {
-    renderWithContext(<TOSLayout />);
-  });
-});
+// describe('test suite for TOSLayout', () => {
+//   test('test render TosLayout', () => {
+//     renderWithContext(<TOSLayout />);
+//   });
+// });
